1c7d3ea978fb6eaa9ff8f0e5b86a11c331e41865
DongKey03 <dongpham24703@gmail.com>
2025-03-11 11:04:16 +0700

    update file color

---
+++ lib/configs/color.dart
@@ -0,0 +1,9 @@
+// color.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Màu sắc chủ đạo
+  static const Color primaryColor = Color(0xFFB0BEC5); // Grey light
+  static const Color hintColor = Color(0xFFFF9800);  // Màu cam làm gợi ý
+  static const Color backgroundColor = Color(0xFFF5F5F5); // Màu nền sáng
+}
